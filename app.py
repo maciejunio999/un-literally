ac6a534b4fd1a60d6d5e7689ca34af9fa9d7f804
Maciej Antosz <maciejantosz13@gmail.com>
2024-12-04 22:56:42 +0100

    wstepne ustawianie word of literally

--- app.py
+++ app.py
@@ -445,14 +445,12 @@ def edit_word(id):
 
     if request.method == 'POST':
         try:
-            # Update only editable fields
             word_to_edit.searched = request.form.get('searched', word_to_edit.searched)
             word_to_edit.definition = request.form.get('definition', word_to_edit.definition)
+            word_to_edit.source += f"Also changed by {session['username']}"
 
-            # Commit the changes to the database
             db.session.commit()
 
-            # Create a new event in the History table
             new_event = History(action=f'Edited word: {word_to_edit.content}', user=session['username'])
             db.session.add(new_event)
             db.session.commit()
@@ -475,6 +473,33 @@ def edit_word(id):
             return render_template('error_page.html', message=f"[?] There was an issue updating this word: {str(e)}")
     else:
         return render_template('edit_word.html', word=word_to_edit)
+
+
+@app.route('/word_of_literally/<int:id>')
+@login_required
+def word_of_literally(id):
+    word_to_edit = Word.query.get_or_404(id)
+    if word_to_edit:
+        word_to_edit.last_as_word_of_literally = datetime.utcnow()
+        try:
+            db.session.commit()
+            return render_template('loading_page.html')
+        except Exception as e:
+            db.session.rollback()
+            payload = {
+                "error": str(e),
+                "word_id": id,
+                "username": session.get('username', 'unknown'),
+                "timestamp": datetime.utcnow().isoformat()
+            }
+            try:
+                response = requests.post("http://127.0.0.1:80/log_exception", json=payload)
+                response.raise_for_status()
+            except requests.exceptions.RequestException as req_e:
+                current_app.logger.error(f"Failed to make a call to logging endpoint: {req_e}")
+            return render_template('error_page.html', message="[?] There was an issue adding word as LWL")
+    else:
+        return render_template('error_page.html', message="[!] There is something wrong with this word")
 
 
 # PROPOSALS
@@ -685,7 +710,7 @@ def show_word(id):
 @app.route('/history', methods=['GET'])
 @login_required
 def history():
-    events = History.query.order_by(History.date).all()
+    events = History.query.order_by(History.date.desc()).all()
     return render_template('history.html', events=events)
 
 
